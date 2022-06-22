18f04102360272ba4f50f89d25a7c89c5f50981d
Elián <elian.fabian.el.belaizi@gmail.com>
2022-06-22 23:06:21 +0200

    Added DontDestroyOnReload.cs

---
+++ Assets/Scripts/Util/DontDestroyOnReload.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+// Be carefull, avoid making this GameObject a child of another
+// if that object is destroyed this one will be too.
+
+/// <summary>
+/// The GameObject won't be destroyed when reloading but it will
+/// when changing to a different scene.
+/// </summary>
+public class DontDestroyOnReload : MonoBehaviour
+{
+    static DontDestroyOnReload Instance;
+
+    Scene initialScene;
+
+    private void Awake()
+    {
+        initialScene = SceneManager.GetActiveScene();
+
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else Destroy(gameObject);
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        var currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene != initialScene) Destroy(gameObject);
+    }
+}
