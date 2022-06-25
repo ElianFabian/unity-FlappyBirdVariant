8c560d08916e5a292be4106bc9161f34d6030eaf
Elián <elian.fabian.el.belaizi@gmail.com>
2022-06-25 15:50:18 +0200

    Removed unncesary RigidBody2D from DeathZone

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -8,25 +8,19 @@ namespace Assets.Scripts
 {
     [DisallowMultipleComponent]
     [RequireComponent(typeof(BoxCollider2D))]
-    [RequireComponent(typeof(Rigidbody2D))]
     public class DeathZone : MonoBehaviour
     {
         public static event Action<Player, Collider2D> OnPlayerCollided;
 
         BoxCollider2D _boxCollider2D;
-        Rigidbody2D   _rigidbody2D;
 
 
 
         private void Awake()
         {
             _boxCollider2D = GetComponent<BoxCollider2D>();
-            _rigidbody2D   = GetComponent<Rigidbody2D>();
-
 
             _boxCollider2D.isTrigger = true;
-
-            _rigidbody2D.isKinematic = true;
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
